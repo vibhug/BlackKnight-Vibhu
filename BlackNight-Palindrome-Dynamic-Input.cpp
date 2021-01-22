45f730478431fbe5f29764a82b274b1d2e2853e0
vibhu <vibhu.gup@gmail.com>
2021-01-22 16:27:37 +0530

    Add files via upload
    Palindrome program is modified to take dynamic input in a loop, until an empty string is passed.
    It will print a message whether input string is ( or is not ) a palindrome.

---
+++ BlackNight-Palindrome-Dynamic-Input.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+bool
+IsPalindrome(const string s)
+{
+	int len = s.length();
+	for (int i = 0; i < len/2; i++) {
+		char left = toupper(s[i]);
+		char right = toupper(s[len -1- i]);
+		if (left == right)
+			continue;
+		else
+			return false;
+	}
+	return true;
+}
+
+int
+main()
+{	
+	cout << "Enter the string to be checked for Palindrome\n";
+	string s;
+	while (std::getline(cin, s)) {
+		if (s.empty())
+			break;
+
+		bool result = IsPalindrome(s);
+
+		string ans =  result? "Is a" : "Is NOT a";
+		std::cout << " Input String:  \" "   << s <<  " \"  " << ans << " palindrome." << '\n';
+	}
+	return 0;
+}
